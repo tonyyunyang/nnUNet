546d68fc67dde8267e9365a7e2ebf294ae8282d0
tony <tonyyunyang@outlook.com>
2025-03-06 13:44:17 +0100

    backup for a big adjustment

---
+++ nnunetv2/training/nnUNetTrainer/variants/pruning/dynamic_net_trainer.py
@@ -0,0 +1,19 @@
+import torch
+
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+
+
+class FlexibleTrainerV1(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        """used for debugging plans etc"""
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.initial_lr = 1e-2
+        self.weight_decay = 3e-5
+        self.oversample_foreground_percent = 0.33
+        self.probabilistic_oversampling = False
+        self.num_iterations_per_epoch = 250
+        self.num_val_iterations_per_epoch = 50
+        self.num_epochs = 1000
+        self.current_epoch = 0
+        self.enable_deep_supervision = True
