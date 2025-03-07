f3cf55762077efa7523e5709ac5eabfebad943b4
tony <tonyyunyang@outlook.com>
2025-03-07 14:45:43 +0100

    small change

--- push_to_forks.sh
+++ push_to_forks.sh
@@ -5,6 +5,6 @@ echo "Pushing dynamic_architecture to dynamic-network-architectures repo..."
 git subtree push --prefix=dynamic_architecture git@github.com:tonyyunyang/dynamic-network-architectures.git main
 
 echo "Pushing nnunetv2 to nnUNet repo..."
-git subtree push --prefix=nnunetv2 git@github.com:tonyyunyang/nnUNet.git master
+git subtree push --prefix=nnUNet git@github.com:tonyyunyang/nnUNet.git master
 
 echo "All subtrees pushed successfully!"
